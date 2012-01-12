21e680cfc661d05f84d17a733e9b8418fc1f334e
tchak <paul@chavard.net>
2012-01-12 10:17:46 +0100

    Rewrite of module addon using Ember.StateManager

--- packages/ember-module/lib/core.js
+++ packages/ember-module/lib/core.js
@@ -1,13 +1,147 @@
-var get = Ember.get, set = Ember.set, getPath = Ember.getPath,
+var get = Ember.get, set = Ember.set, getPath = Ember.getPath, setPath = Ember.setPath,
   slice = Array.prototype.slice, fmt = Ember.String.fmt;
 
-Ember.Module = Ember.Object.extend({
-  isReady: false,
-  isLoaded: false,
-  isLoading: false,
-  isPrefetched: false,
+Ember.$.ajaxSetup({
+  converters: {
+    '* module': true
+  }
+});
 
+Ember.$.ajaxPrefilter("module", function(options, originalOptions, xhr) {
+  if (!options.crossDomain) {
+    xhr.deferred = true;
+    return 'text';
+  } else {
+    return 'script';
+  }
+});
+
+var stateProperty = Ember.computed(function(key) {
+  var parent = get(this, 'parentState');
+  if (parent) {
+    return get(parent, key);
+  }
+}).property();
+
+var moduleState = Ember.State.extend({
+  isReady: stateProperty,
+  isLoaded: stateProperty,
+  isLoading: stateProperty,
+  isPrefetching: stateProperty,
+  isError: stateProperty
+});
+
+var fetch = function(stateManager, execute) {
+  $.holdReady(true);
+  var module = get(stateManager, 'module');
+  Ember.$.ajax(get(module, 'url'), {
+    dataType: 'module',
+    success: function(data, textStatus, xhr) {
+      console.log(xhr.deferred, execute);
+      if (xhr.deferred) {
+        set(module, 'source', data);
+      }
+      if (execute) {
+        stateManager.goToState('loaded.ready');
+      } else {
+        stateManager.goToState('loaded');
+      }
+    },
+    error: function() { stateManager.goToState('error'); },
+    complete: function() { $.holdReady(false); }
+  });
+};
+
+var states = {
+  rootState: Ember.State.create({
+    isReady: false,
+    isLoaded: false,
+    isLoading: false,
+    isPrefetching: false,
+    isError: false,
+
+    load: function(stateManager) {
+      stateManager.goToState('loading');
+    },
+
+    prefetch: function(stateManager) {
+      stateManager.goToState('prefetching');
+    },
+
+    prefetching: moduleState.create({
+      isPrefetching: true,
+      isLoading: true,
+      load: Ember.K,
+      enter: function(stateManager) {
+        fetch(stateManager);
+      }
+    }),
+
+    loading: moduleState.create({
+      isLoading: true,
+      prefetch: Ember.K,
+      enter: function(stateManager) {
+        fetch(stateManager, true);
+      }
+    }),
+
+    loaded: moduleState.create({
+      isLoaded: true,
+      load: function(stateManager) {
+        stateManager.goToState('loaded.ready');
+      },
+      prefetch: Ember.K,
+      ready: moduleState.create({
+        isReady: true,
+        enter: function(stateManager) {
+          var module = get(stateManager, 'module'), source = get(module, 'source');
+          if (typeof source === 'string') {
+            jQuery.globalEval(source);
+            setPath(stateManager, 'module.source', null);
+          }
+          module.invokeCallbacks();
+        }
+      })
+    }),
+
+    error: moduleState.create({
+      isError: true
+    })
+  })
+};
+
+var moduleStateManager = Ember.StateManager.extend({
+  module: null,
+  initialState: 'rootState',
+  states: states
+});
+
+var retrieveFromCurrentState = Ember.computed(function(key) {
+  return get(getPath(this, 'stateManager.currentState'), key);
+}).property('stateManager.currentState').cacheable();
+
+Ember.Module = Ember.Object.extend({
+
+  isReady: retrieveFromCurrentState,
+  isLoaded: retrieveFromCurrentState,
+  isLoading: retrieveFromCurrentState,
+  isPrefetching: retrieveFromCurrentState,
+
+  isPrefetched: false,
   prefetchDelay: 500,
+
+  init: function() {
+    this._super();
+    var stateManager = moduleStateManager.create({
+      module: this
+    });
+
+    set(this, 'stateManager', stateManager);
+
+    if (get(this, 'isPrefetched')) {
+      Ember.run.later(this, 'prefetch', get(this, 'prefetchDelay'));
+    }
+  },
 
   /**
    * [load description]
@@ -16,54 +150,19 @@ Ember.Module = Ember.Object.extend({
    * @return {[type]}
    */
   load: function(target, method) {
-    var args = slice.call(arguments, 2), _this = this, moduleDidLoad;
+    var stateManager = get(this, 'stateManager'),
+        args;
 
-    // Treat the first parameter as the callback if the target is a function and there is
-    // no method supplied.
     if (method === undefined && Ember.typeOf(target) === 'function') {
       method = target;
       target = null;
-    }
-
-    moduleDidLoad = function(module) {
-      if (module === _this) {
-        amplify.unsubscribe('ember:modules:ready', moduleDidLoad);
-        if (method) { method.apply(this, args); }
-      }
-    };
-    amplify.subscribe('ember:modules:ready', target, moduleDidLoad);
-
-    get(this, 'isPrefetching', false);
-    if (get(this, 'isLoaded')) {
-      this.execute();
-      return true;
+      args = slice.call(arguments, 1);
     } else {
-      this.fetch();
-      return false;
+      args = slice.call(arguments, 2);
     }
-  },
 
-  /**
-   * [fetch description]
-   * @return {[type]}
-   */
-  fetch: function() {
-    if (get(this, 'isLoading') || get(this, 'isLoaded')) return;
-    set(this, 'isLoading', true);
-    $.holdReady(true);
-    Ember.$.ajax(get(this, 'url'), {
-      dataType: 'script',
-      context: this,
-      success: function() {
-        set(this, 'isLoaded', true);
-        if (!get(this, 'isPrefetching')) { this.execute(); }
-      },
-      complete: function() {
-        set(this, 'isLoading', false);
-        get(this, 'isPrefetching', false);
-        $.holdReady(false);
-      }
-    });
+    if (method) { this.addCallback(target, method, args); }
+    stateManager.send('load', stateManager);
   },
 
   /**
@@ -71,47 +170,52 @@ Ember.Module = Ember.Object.extend({
    * @return {[type]}
    */
   prefetch: function() {
-    if (get(this, 'isLoading') || get(this, 'isLoaded')) return;
-    get(this, 'isPrefetching', true);
-    Ember.run.later(this, 'fetch', get(this, 'prefetchDelay'));
+    var stateManager = get(this, 'stateManager');
+    stateManager.send('prefetch', stateManager);
   },
 
-  /**
-   * [execute description]
-   * @return {[type]}
-   */
-  execute: function() {
-    if (!get(this, 'isReady')) {
-      var moduleSource = get(this, 'source');
-      if (typeof moduleSource === 'string') {
-        jQuery.globalEval(moduleSource);
-        set(this, 'source', null);
-      }
-      set(this, 'isReady', true);
-    }
+  addCallback: function(target, method, args) {
+    this._callbacks.push({
+      target: target,
+      method: method,
+      args: args
+    });
+  },
 
-    amplify.publish('ember:modules:ready', this);
-  }
+  invokeCallbacks: function() {
+    var i, l, cb, cbs = this._callbacks;
+    for (i = 0, l = cbs.length; i < l; i++) {
+      cb = cbs[i];
+      cb.method.apply(cb.target, cb.args);
+    }
+    this._callbacks = [];
+  },
+
+  _callbacks: []
 });
 
 Ember.ModulesManager = Ember.Object.extend({
   init: function() {
     this._super();
-    this._initializeModules();
-  },
 
-  _initializeModules: function() {
-    var modules = get(this, 'modules'), module,
-      prefetchedModules = [], key;
+    var modules = get(this, 'modules'), key;
     if (modules) {
       for (key in modules) {
-        module = modules[key];
-        if (Ember.typeOf(module) === 'object') {
-          module = Ember.Module.create({name: key, modulesManager: this}, module);
-          modules[key] = module;
-          if (module.isPrefetched) { module.prefetch(); }
-        }
+        this.createModule(key, modules[key]);
       }
+    }
+  },
+
+  /**
+   * [createModule description]
+   * @param  {[type]} moduleName [description]
+   * @param  {[type]} module     [description]
+   * @return {[type]}
+   */
+  createModule: function(moduleName, module) {
+    if (typeof moduleName === 'string' && module && typeof module.url === 'string') {
+      module = Ember.Module.create({name: moduleName, modulesManager: this}, module);
+      setPath(this, 'modules.%@'.fmt(moduleName), module);
     }
   },
 
@@ -153,14 +257,11 @@ Ember.ModulesManager = Ember.Object.extend({
     @returns {Boolean}
   */
   isModuleReady: function(moduleName) {
-    var module = getPath(this, 'modules.'+moduleName);
-    return module ? !!module.isReady : false;
+    return !!getPath(this, 'modules.'+moduleName+'.isReady');
   }
 });
 
 Ember.$(function() {
-  if (Ember.MODULES) {
-    Ember.modules = Ember.ModulesManager.create({modules: Ember.MODULES});
-    delete Ember.MODULES;
-  }
+  Ember.modules = Ember.ModulesManager.create({modules: Ember.MODULES || {}});
+  delete Ember.MODULES;
 });
